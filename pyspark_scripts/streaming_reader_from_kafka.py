8af89a5e910988fd971d2c4d2ed8bb3e7e3f1efd
Prashanth Sriram <prashanthsriram145@gmail.com>
2023-04-26 22:05:26 +0530

    Changes to write to Mongodb collection

--- pyspark_scripts/streaming_reader_from_kafka.py
+++ pyspark_scripts/streaming_reader_from_kafka.py
@@ -10,7 +10,12 @@ def streaming_read_from_kafka():
     structFields.append(StructField('age', IntegerType(), True))
     schema = StructType(structFields)
 
-    spark = SparkSession.builder.appName('streaming_reader_from_kafka').master("local").getOrCreate()
+    spark = SparkSession.builder.appName('streaming_reader_from_kafka').master("local") \
+        .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/learning") \
+        .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/learning") \
+        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
+        .getOrCreate()
+
     df = spark.readStream \
         .format('kafka') \
         .option('kafka.bootstrap.servers', 'localhost:29092') \
@@ -21,10 +26,21 @@ def streaming_read_from_kafka():
 
     df2 = df1.select(from_json(col("json"), schema=schema).alias("json")).select("json.*")
 
-    df2.writeStream.format("json") \
-        .outputMode("append") \
+    # df2.writeStream.format("json") \
+    #     .outputMode("append") \
+    #     .option('checkpointLocation', '../checkpoint') \
+    #     .option('path', "../output") \
+    #     .start() \
+    #     .awaitTermination()
+
+    df2.writeStream \
+        .format("mongodb") \
         .option('checkpointLocation', '../checkpoint') \
-        .option('path', "../output") \
+        .option("spark.mongodb.connection.uri", "mongodb://127.0.0.1:27017/") \
+        .option("spark.mongodb.change.stream.publish.full.document.only", "true") \
+        .option("spark.mongodb.database", "learning") \
+        .option("spark.mongodb.collection", "persons") \
+        .outputMode("append") \
         .start() \
         .awaitTermination()
 
